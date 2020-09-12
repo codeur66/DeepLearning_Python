2a3046c2b0bfeb65129e06f8356a17eabee0c0b4
nikos <baroutisnk@gmail.com>
2020-09-12 11:23:56 +0300

    started the actions to create the architecture of the NN, the three layers

---
+++ computer_vision/a_small_convnet/model.py
@@ -0,0 +1,16 @@
+# Conv2D and MaxPooling2D layers
+# Listing 5.1
+from keras import layers
+from keras import models
+
+# Sequential groups a linear stack of layers into a `tf.keras.Model`.
+# Sequential provides training and inference features on this model.
+model = models.Sequential()
+
+model.add(layers.Conv2D(32, (3, 3)), activate='relu', input_shape=(28, 28, 1))
+# It is basically used to reduce the size of the image because the larger number
+# of pixels contribute to more parameters which can involve large chunks of data.
+# Thus we need less parameters such that a CNN can still identify the image.
+# or choose the Average value
+model.add(layers.MaxPool2D(2, 2))
+
