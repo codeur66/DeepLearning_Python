04cd873a1d076cd77027fc922f80ee4f526ca3e7
nikos <baroutisnk@gmail.com>
2020-09-12 11:30:19 +0300

    test git add , avoid the prints

--- computer_vision/a_small_convnet/model.py
+++ computer_vision/a_small_convnet/model.py
@@ -7,10 +7,17 @@ from keras import models
 # Sequential provides training and inference features on this model.
 model = models.Sequential()
 
+# Images are in RGB => three features => three layers.
+
 model.add(layers.Conv2D(32, (3, 3)), activate='relu', input_shape=(28, 28, 1))
-# It is basically used to reduce the size of the image because the larger number
+# MaxPool2D: It is basically used to reduce the size of the image because the larger number
 # of pixels contribute to more parameters which can involve large chunks of data.
 # Thus we need less parameters such that a CNN can still identify the image.
 # or choose the Average value
 model.add(layers.MaxPool2D(2, 2))
+
+model.add(layers.Conv2D())
+model.add(layers.MaxPool2D())
+model.add(layers.Conv2D())
+model.add(layers.MaxPool2D())
 
