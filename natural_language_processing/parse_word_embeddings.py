17ad5a7140e2455ae48c1220905af2eea159c0c4
nikos <baroutisnk@gmail.com>
2020-10-17 23:43:42 +0300

    better commenting code

--- natural_language_processing/parse_word_embeddings.py
+++ natural_language_processing/parse_word_embeddings.py
@@ -1,9 +1,17 @@
 import os
 import numpy as np
 from natural_language_processing.sentiment_data_processing import TextProcessing
-"""In contrast to TextProcessing which holds external data
-ParseWordEmbeddings is  word embeddings (pretrained / the output of Neural Network) 
-and not flexible to changes or extension.
+
+"""In contrast to TextProcessing class which holds external data
+ParseWordEmbeddings is a pretrained  network. A saved network that was previously
+trained on a large dataset.If this original dataset is large enough and general enough
+the hierarchy of features learned by the pretrained network can effectively act as 
+a generic model.But for very specific tasks word embeddings make things worse.
+It is flexible to changes for fine tunning unfreezing a few of
+the top layers of a frozen model, which hold the more abstract patterns in order 
+to make them more relevant for the problem at hand, to improve the accuracy around 1%.
+But we avoid to spend so much time, so we do not change the class, so we make use of
+classmethod decorator.
 """
 
 class ParseWordEmbeddings:
@@ -14,8 +22,7 @@ class ParseWordEmbeddings:
     glove_dir = config.external_data_sources.word_embeddings
     file_name = config.external_data_sources.embeddings_file_name
 
-
-    # embeddings have coefficients of similarity
+    # embeddings have coefficients of similarity creating he word vectors
     @classmethod
     def create_embeddings_index(cls):
         embeddings_index = {}
@@ -50,8 +57,6 @@ class ParseWordEmbeddings:
                 if embedding_vector is not None:
                     embedding_matrix[i] = embedding_vector
         print("embedding_matrix : ", embedding_matrix)
-
-
 
 
 
