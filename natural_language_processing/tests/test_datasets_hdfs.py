9463aad03b490991b37f843530f0a571d1bb2864
nikos <baroutisnk@gmail.com>
2020-11-16 10:44:12 +0200

    unittest_datasets_completed

--- natural_language_processing/tests/test_datasets_hdfs.py
+++ natural_language_processing/tests/test_datasets_hdfs.py
@@ -2,33 +2,39 @@ import unittest
 import natural_language_processing.model.read_hdf5 as rd
 
 
-class MyTestCase(unittest.TestCase):
-    print("Tests if every dataset is not empty.")
-    x_trnset, y_trnset, x_valset, y_valset, hdf_in = rd.get_internal_hdf()
-    embeddings, hdf_ext = rd.get_external_hdf()
+class TestDatasets(unittest.TestCase):
+    print("Tests if every dataset type is not empty and is ndarray.")
+
+    def setUp(self) -> None:
+        self.x_trnset, self.y_trnset, self.x_valset, self.y_valset, self.hdf_in = rd.get_internal_hdf()
+        self.embeddings, self.hdf_ext = rd.get_external_hdf()
 
     def test_noneType_internal(self):
-        if type(MyTestCase.x_trnset) is type(None) or \
-                type(MyTestCase.y_trnset) is type(None) or \
-                type(MyTestCase.x_valset) is type(None) or \
-                type(MyTestCase.y_valset) is type(None):
+        if type(self.x_trnset) is type(None) or \
+                type(self.y_trnset) is type(None) or \
+                type(self.x_valset) is type(None) or \
+                type(self.y_valset) is type(None):
             raise Exception("NoneType in internal dataset is not permitted.Every test in datasets stops.")
 
     def test_noneType_external(self):
-        if type(MyTestCase.embeddings) is type(None):
+        if type(self.embeddings) is type(None):
             raise Exception("NoneType in external dataset is not permitted.Every test in datasets stops.")
 
     def test_datatype_get_internal_hdf(self):
-        self.assertEqual(type(MyTestCase.x_trnset[:]).__name__, 'ndarray')
-        self.assertEqual(type(MyTestCase.y_trnset[:]).__name__, 'ndarray')
-        self.assertEqual(type(MyTestCase.x_valset[:]).__name__, 'ndarray')
-        self.assertEqual(type(MyTestCase.y_valset[:]).__name__, 'ndarray')
-        self.assertEqual(str(MyTestCase.hdf_in), '<HDF5 file "internal_dataset.h5py" (mode r)>')
+        self.assertEqual(type(self.x_trnset[:]).__name__, 'ndarray')
+        self.assertEqual(type(self.y_trnset[:]).__name__, 'ndarray')
+        self.assertEqual(type(self.x_valset[:]).__name__, 'ndarray')
+        self.assertEqual(type(self.y_valset[:]).__name__, 'ndarray')
+        self.assertEqual(str(self.hdf_in), '<HDF5 file "internal_dataset.h5py" (mode r)>')
 
     def test_datatype_get_external_hdf(self):
-        self.assertEqual(type(MyTestCase.embeddings[:]).__name__, 'ndarray')
-        self.assertEqual(str(MyTestCase.hdf_ext), '<HDF5 file "external_dataset.h5py" (mode r)>', "HEY")
+        self.assertEqual(type(self.embeddings[:]).__name__, 'ndarray')
+        self.assertEqual(str(self.hdf_ext), '<HDF5 file "external_dataset.h5py" (mode r)>', "HEY")
 
+
+    def tearDown(self) -> None:
+        self.hdf_in.close()
+        self.hdf_ext.close()
 
 if __name__ == '__main__':
     unittest.main()
