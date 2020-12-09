d7f092a6042632a0412f9b9316dc790af5198567
nikos <baroutisnk@gmail.com>
2020-12-09 23:33:05 +0200

    Jenkins CI Pipeline, test coverage

--- natural_language_processing/configurations/configuration_infrastructure.py
+++ natural_language_processing/configurations/configuration_infrastructure.py
@@ -15,7 +15,6 @@ class Config:
     @classmethod
     def from_json(cls, cfg):
         """Creates config from json"""
-        import tensorflow.keras.optimizers  as tf
         # params = json.loads(json.dumps(cfg, default = lambda o: o.__dict__), object_hook=HelperObject)
         params = json.loads(json.dumps(cfg), object_hook=HelperObject)
         return cls(params.external_data_sources, params.data, params.train, params.model)
