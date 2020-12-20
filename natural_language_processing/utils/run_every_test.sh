61f18641b5cf580972cecb3f4814f19ee50acc15
nikos <baroutisnk@gmail.com>
2020-12-20 20:17:26 +0200

    jenkinsfile

--- natural_language_processing/utils/run_every_test.sh
+++ natural_language_processing/utils/run_every_test.sh
@@ -1,5 +1,5 @@
 #for file in /home/nikoscf/PycharmProjects/DeepLearningWithPython/natural_language_processing/tests/*.py ;
 #do python "$file";done
 
-for file in natural_language_processing/tests/*.py ;
+for file in tests/*.py ;
 do python "$file";done
